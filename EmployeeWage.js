4ab64eb185dc196f5278696b9d928d6922067d09
sharale34 <sourabhharale8000@gmail.com>
2020-11-12 12:08:24 +0530

    Added: Performing operations using Array Helper Functions

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -32,8 +32,8 @@
                 return 0;
         }
     }
-    
-    function calcDailyWage(empHrs){
+
+    function calcDailyWage(empHrs) {
         return empHrs * WAGE_PER_HOUR;
     }
 
@@ -50,5 +50,31 @@
         empDailyWageArr.push(calcDailyWage(empHrs));
     }
     let empWage = calcDailyWage(totalEmpHrs);
-    console.log("Total days worked " + totalWorkingDays + " Total hours worked " + totalEmpHrs + " Employee Wage " + empWage);
+    console.log("UC-6 Total days worked " + totalWorkingDays + " Total hours worked " + totalEmpHrs + " Employee Wage " + empWage);
+
+    //Array Helper Functions
+    //UC 7A - Calculate total wage using Array forEach traversal 
+    let totEmpWage = 0;
+    function sum(dailyWage) {
+        totEmpWage += dailyWage;
+    }
+    empDailyWageArr.forEach(sum); //forEach calls the function sum one time for each element in the array
+    console.log("UC-7A Total Days Worked: " + totalWorkingDays + " Total Hrs Worked " + totalEmpHrs + " Emp Wage " + totEmpWage);
+
+    //UC 7A - Calculate total wage using Array reduce method
+    function totalWages(totalWage, dailyWage) {
+        return totalWage + dailyWage;
+    }
+    //The return value of the function is the accumulated result, and is provided as an argument in the next call to the function.
+    console.log("UC-7A Emp Wage with reduce: " + empDailyWageArr.reduce(totalWages, 0));
+
+    // UC-7B Show the Day along with Daily Wage using Array Map helper function
+    let dailyCounter = 0;
+    function mapDayWithWage(dailyWage) {
+        dailyCounter++;
+        return "Day-" + dailyCounter + " = " + dailyWage;
+    }
+    let empDayWithWageArr = empDailyWageArr.map(mapDayWithWage);
+    console.log("UC-8 Daily Wage Map");
+    console.log(empDayWithWageArr);
 }
